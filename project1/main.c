1afef45176743fdad391b9f1c7ea7b81e1e9b208
sindusri <mankenasindhu1708@gmail.com>
2019-01-26 17:42:18 -0600

    committing project 1

---
+++ project1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+int i,j,k;
+printf("Enter number i:");
+scanf("%d",&i);
+
+printf("Enter number j:");
+scanf("%d",&j);
+if(i<=0 || j<=0 ){
+printf("i or j less than or equal to zero\n");
+}
+else{
+printf("sum of two numbers: %d",k=i+j);
+    }
+return 0;
+
+}
